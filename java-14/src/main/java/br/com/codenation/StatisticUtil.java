eab0a2a6a696c9cec45651a08e91af6792b2e554
laurgirdwood <azevedoemello@gmail.com>
2020-05-18 22:33:56 -0300

    ajuste no método que calcula moda. submit 100%

--- java-14/src/main/java/br/com/codenation/StatisticUtil.java
+++ java-14/src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,23 +1,26 @@
 package br.com.codenation;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class StatisticUtil {
 
 	public static int average(int[] elements) {
-		int sum = 0;
-		int elementsSize = elements.length;
-		for (int element : elements) {
-			sum += element;
-		}
-		return sum/elementsSize;
+		return (int) Arrays.stream(elements).average().orElse(0.0);
 	}
 
 	public static int mode(int[] elements) {
 		int mode = 0;
+		int countMax = 0;
+		int count = 0;
+
 		for (int element : elements) {
-			for (int j = 0; j < elements.length - 1; j++) {
-				if (element == elements[j]) {
+			count = 0;
+			for (int elementComp : elements) {
+				count = element == elementComp ? count + 1 : count;
+				if (count > countMax) {
+					countMax = count;
 					mode = element;
-					break;
 				}
 			}
 		}
@@ -25,11 +28,13 @@ public class StatisticUtil {
 	}
 
 	public static int median(int[] elements) {
-		int elementsSize = elements.length;
-		if(elementsSize % 2 != 0) {
-			return elements[(elementsSize - 1)/2];
+		Arrays.sort(elements);
+		int meio = elements.length/2;
+
+		if(elements.length % 2 != 0) {
+			return elements[meio];
 		} else {
-			return elements[elementsSize/2] + elements[((elementsSize/2) + 1)/ 2];
+			return (elements[(meio) - 1] + (elements[meio])) / 2;
 		}
 	}
 }
